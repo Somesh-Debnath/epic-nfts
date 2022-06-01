9d70c787944f57c6ec20c060280cbb3098af01a4
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2022-06-01 14:17:11 +0530

    implemented openzeppelin and created test NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,29 @@
-async function main(){
-    const nftContract=await hre.ethers.getContractFactory("MyEpicNFT");
-const deployedNftContract=await nftContract.deploy();
-await deployedNftContract.deployed();
-console.log("Nft Contract deployed at:",deployedNftContract.address)
-}
-const runMain=async()=>{
-try{
-   await main()
-    process.exit(0)
-}catch(error){
-    console.log(error)
-    process.exit(1)
-}
-}
-
-runMain()
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+  
+    // Mint another NFT for fun.
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+  
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
